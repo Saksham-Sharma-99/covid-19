bf56164eb62778f098b46495c6c126ba5344911a
Saksham Sharma <saksham@go-gaga-app.com>
2020-09-12 23:54:46 +0530

    no message

--- API.js
+++ API.js
@@ -6,17 +6,35 @@ module.exports.helpLineNumbers = helpLineNumbers;
 
 
 
+var contactsRegional = []
+function contactsR(loc,number){
+  this.loc = loc;
+  this.number = number;
+}
+
 function helpLineNumbers(){
   return makeReq("contacts")
 }
 
 
+
+
 function makeReq(endPoint){
-  https.get(baseUrl+endPoint,function(response){
-    response.on("data",function(data){
-      const d = JSON.parse(data);
-      console.log(d);
-      return d;
-    })
+  https.get(baseUrl+endPoint,"JSON", function(response){
+    var data;
+
+    response.on("data", function(chunk) {
+      if (!data) {
+        data = chunk;
+      } else {
+        data += chunk;
+      }
+    });
+
+    response.on("end", function() {
+        const d=JSON.parse(data);
+        console.log(d.data.contacts);
+        
+    });
   })
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-// const api = require(__dirname + "/API.js")
+const api = require(__dirname + "/API.js")
 
 const app = express();
 app.use(express.static("public"));
@@ -11,7 +11,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
   res.send("home");
-  // api.helpLineNumbers();
+  var contacts = api.helpLineNumbers();
+  console.log(contacts);
 });
 
 
