fe30a3777b07403b7ad4f8dd055f499fe5cc9f39
Saksham Sharma <saksham@go-gaga-app.com>
2020-09-14 04:40:21 +0530

    fonts added

--- public/scroll.js
+++ public/scroll.js
@@ -1,13 +1,17 @@
-$(document).ready(function($){
+$(document).ready(function($) {
   var $el = $(".table-responsive");
-function anim() {
-var st = $el.scrollTop();
-var sb = $el.prop("scrollHeight")-$el.innerHeight();
-$el.animate({scrollTop: st<sb/2 ? sb : 0}, 90000, anim);
-}
-function stop(){
-$el.stop();
-}
-anim();
-$el.hover(stop, anim);
+
+  function anim() {
+    var st = $el.scrollTop();
+    var sb = $el.prop("scrollHeight") - $el.innerHeight();
+    $el.animate({
+      scrollTop: st < sb / 2 ? sb : 0
+    }, 90000, anim);
+  }
+
+  function stop() {
+    $el.stop();
+  }
+  anim();
+  $el.hover(stop, anim);
 })
