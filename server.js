f46bd1f908dd77191589bdca387f0a04b47478c1
Saksham Sharma <saksham@go-gaga-app.com>
2020-09-13 13:06:34 +0530

    navbar is functional , server setup partially done

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
+const alert = require("alert");
 const api = require(__dirname +"/API.js")
 
 const app = express();
@@ -10,14 +12,59 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+//GET
 app.get("/", function(req, res) {
   res.render("home");
   api.getNotifications(function(n){
     console.log(n);
   })
 });
+app.get("/notifs",function(req,res){
+  res.render("notifications");
+})
+app.get("/dashboard/:name",function(req,res){
+  res.render("dashboard");
+});
 
 
+
+
+
+
+
+//POST
+app.post("/dashboard",function(req,res){
+  console.log(req.body);
+  api.getDashboardData(req.body.stateName,function(dashboardData){
+    console.log(dashboardData);
+    res.redirect("/dashboard/"+lodash.kebabCase(req.body.stateName))
+  })
+});
+
+app.post("/",function(req,res){
+  api.getContacts(function(regional,primary){
+    var stateName = ""
+    var contact = "No Such State Exists in India";
+    for(var i = 0 ; i<regional.length ; i++){
+      if(lodash.kebabCase(req.body.stateName) === lodash.kebabCase(regional[i].loc)){
+        contact = regional[i].number;
+        stateName = regional[i].loc;
+      }
+    }
+    if (stateName!= ""){
+    alert("Emergency Contact for "+stateName+ " is : " + contact)
+    res.redirect("/");
+  }else{
+    alert(contact)
+    res.redirect("/");
+  }
+});
+});
+
+
+
+
+//PORT Setup
 app.listen(3000, function() {
   console.log("Started listening Sucessfully");
-})
+});
